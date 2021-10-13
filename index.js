364e38556fcb167aefaaf1fe1a9decb0593286c6
Rob Sutter <rob.sutter@fauna.com>
2021-10-13 08:59:24 -0400

    Working across global, US, and EU region groups

--- index.js
+++ index.js
@@ -1,43 +1,50 @@
 import faunadb from 'faunadb';
-import {customFetch} from './utils.js';
+import { customFetch } from './utils.js';
+
+const validPaths = [
+  '/',
+  '/udf',
+  '/eu/udf',
+  '/us/udf'
+]
 
 addEventListener('fetch', event => {
-    try {
-        const request = event.request;
-        const url = new URL(request.url);
-        if (url.pathname !== '/') {
-            return event.respondWith(new Response('Not Found', { status: 404 }));
-        } else if (request.method.toUpperCase() === 'POST') {
-            return event.respondWith(handleRequest(request));
-        } else {
-            return event.respondWith(new Response('Method Not Allowed', { status: 405 }));
-        }
-    } catch (e) {
-        return event.respondWith(new Response(e.message, { status: 500 }));
+  try {
+    const request = event.request;
+    const url = new URL(request.url);
+    if (!validPaths.includes(url.pathname.toLowerCase())) {
+      return event.respondWith(new Response('Not Found', { status: 404 }));
+    } else if (request.method.toUpperCase() === 'POST') {
+      return event.respondWith(handleRequest(request));
+    } else {
+      return event.respondWith(new Response('Method Not Allowed', { status: 405 }));
     }
+  } catch (e) {
+    return event.respondWith(new Response(e.message, { status: 500 }));
+  }
 });
 
 async function handleRequest(request) {
 
-    try {
-        const secret = request.headers.get('X-Fauna-Secret');
-        const body = await request.json();
-        const udf = body.function;
-        const args = body.arguments;
+  try {
+    const secret = request.headers.get('X-Fauna-Secret');
+    const body = await request.json();
+    const udf = body.function;
+    const args = body.arguments;
 
-        const faunaClient = new faunadb.Client({
-            secret: secret,
-            fetch: customFetch
-        });
+    const faunaClient = new faunadb.Client({
+      secret: secret,
+      fetch: customFetch
+    });
 
-        const { Call } = faunadb.query;
+    const { Call } = faunadb.query;
 
-        const result = await faunaClient.query(
-            Call(udf, args)
-        );
+    const result = await faunaClient.query(
+      Call(udf, args)
+    );
 
-        return new Response(result, { status: 200 });
-    } catch (e) {
-        return new Response(e.message, { status: 500 });
-    }
+    return new Response(result, { status: 200 });
+  } catch (e) {
+    return new Response(e.message, { status: 500 });
+  }
 }
